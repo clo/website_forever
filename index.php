25ad45c52ccd242cf403ed68763a849ade388320
Christian Lochmatter <christian.lochmatter@gmail.com>
2015-02-28 19:20:59 +0100

    add more sites to display

--- index.php
+++ index.php
@@ -1,40 +1,55 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<title>-Erich Zbinden Hallenturnier 2015</title> 
+<title>Erich Zbinden Hallenturnier 2015</title> 
 <script>
 
+var debug = false;
+var timer=10;
+var count=timer;
 var i=0;
 var webpageArray = [
   // from, to,    ,url
-  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-june'],
-  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-junf'],
-  ['06:50','16:10','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-vorrunde'],
-  ['13:45','15.00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/rangliste-vorrunde'],
-  ['16:20','17:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-final'],
-  ['16:20','18:00','http://localhost/joomla_hallenturnier/index.php/schlussrangliste'],
-  ['05:00','13:00','http://localhost/joomla_hallenturnier/index.php/essensplan'],
+  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen/impressionen-2014/impressionen-june'],
+  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen/impressionen-2014/impressionen-junf'],
+  //['12:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-2015/impressionen-june'],
+  //['12:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-2014/impressionen-junf'],
+  
+  // Sponsoren
+  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/sponsoren'],
+  
+  // Spielbetrieb
+  // Vorrunde
+  ['06:50','22:10','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-vorrunde'],
+  ['13:45','22.00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/rangliste-vorrunde'],
+  
+  // Final
+  ['16:20','22:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-final'],
+  
+  // Schlussrangliste
+  ['16:30','22:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/schlussrangliste'],
+  
+  // Essensplan
+  ['10:00','22:00','http://localhost/joomla_hallenturnier/index.php/essensplan'],
 ];
 var webPageToShow = [];
 
-setInterval(openUrl, 30000); // Wait 10 seconds
-setInterval(buildArray,1000);
+setInterval(openUrl, timer * 1000); // Wait 10 seconds
+setInterval(buildArray,5000);
 function openUrl(){
-   
-   //buildArray();
-   //if (!webPageToShow){
-   //  return false;
-   //}
-   
    console.log('i=' + i + ' = ' + webPageToShow[i]);
    if (i>=webPageToShow.length){
      i=0;
    }
-   var iframe = document.getElementById("myframe");
+   var iframe = document.getElementById('myframe');
    iframe.src = webPageToShow[i];  
-	  
+   if (debug){
+     document.getElementById('debug').innerHTML = webPageToShow[i]; 
+   }
+ 
    i++;
    return false;
+   
 }
 
 function buildArray(){
@@ -49,15 +64,15 @@ function buildArray(){
     var nowMinutes = now.getMinutes();
     var nowInMin = (nowHours) * 60 + nowMinutes;
     
-	var startInMin = calcMin(start);
+    var startInMin = calcMin(start);
     var endInMin = calcMin(end);
     console.log('nowInMin=' + nowInMin+' startInMin='+startInMin+' endInMin='+endInMin);
-	if (nowInMin>=startInMin && nowInMin<=endInMin){
+    if (nowInMin>=startInMin && nowInMin<=endInMin){
       console.log('shown: ' + webpageArray[i][2]);
-	  webPageToShowNew.push(webpageArray[i][2]);
-	}else{
-	  console.log('blocked: ' + webpageArray[i][2]);
-	}
+      webPageToShowNew.push(webpageArray[i][2]);
+    }else{
+      console.log('blocked: ' + webpageArray[i][2]);
+    }
   }
   webPageToShow = webPageToShowNew;
   console.log(webPageToShow);
@@ -74,6 +89,7 @@ function calcMin(time){
 
 </head>
 <body>
+  <p id='debug'></p>
   <iframe seamless="seamless" style="overflow:hidden;" scrolling="no" height='100%' width='100%' id='myframe' src="http://localhost/joomla_hallenturnier" />
 </body>
 </html>
