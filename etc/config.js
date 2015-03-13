aaa68d3011f33d0e83ed048b9976250587aec393
Christian Lochmatter <christian.lochmatter@gmail.com>
2015-03-13 19:54:20 +0100

    add config.js file

---
+++ etc/config.js
@@ -0,0 +1,31 @@
+var webpageArray = [
+  // from, to,    ,url
+  //['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen/impressionen-2014/impressionen-june'],
+  //['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen/impressionen-2014/impressionen-junf'],
+  ['05:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen/impressionen-2015/impressionen-june'],
+  //['12:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-2015/impressionen-june'],
+  //['12:00','22:00','http://localhost/joomla_hallenturnier/index.php/impressionen-2014/impressionen-junf'],
+  
+  // Sponsoren
+  ['05:00','23:00','http://localhost/joomla_hallenturnier/index.php/sponsoren-menu/sponsoren-1'],
+  ['05:00','23:00','http://localhost/joomla_hallenturnier/index.php/sponsoren-menu/sponsoren-2'],
+  
+  // Spielbetrieb
+  // Vorrunde
+  ['08:00','12:40','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-vorrunde'],
+  ['12:40','12:50','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/rangliste-vorrunde'],
+  
+  // Endrunde
+  ['12:40','13:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/gruppeneinteilung-endrunde'],
+  ['12:50','17:10','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-endrunde'],
+  ['17:00','20:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/rangliste-endrunde'],
+  
+  // Final
+  ['17:00','20:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/spielbetrieb-final'],
+  
+  // Schlussrangliste
+  ['17:30','22:00','http://localhost/joomla_hallenturnier/index.php/spielbetrieb/schlussrangliste'],
+  
+  // Essensplan
+  ['10:00','13:00','http://localhost/joomla_hallenturnier/index.php/essensplan'],
+];
